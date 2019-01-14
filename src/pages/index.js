e32b6a109a13cbba198ffee236426f3d7b9e8b14
Tomas Gadliauskas <troliukass@gmail.com>
2019-01-14 23:03:26 +0100

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,88 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import styled from "styled-components";
 
+const StyledPostPreview = styled(Link)`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  flex-direction: row;
+  border-bottom: 1px solid #ddd;
+  padding: 2em 4em;
+  transition: opacity 0.15s linear;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const PostTitleContainer = styled.div`
+  width: 100%;
+  max-width: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PostContentContainer = styled.div`
+  max-width: 30%;
+`;
+
+const PostTitle = styled.span`
+  font-size: 1.75rem;
+  color: black;
+  font-weight: bold;
+`;
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+    console.log(posts);
     return (
       <Layout>
         <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
           </div>
+          {posts.map(({ node: post }) => (
+            <StyledPostPreview key={post.id} to={post.fields.slug}>
+              <PostTitleContainer>
+                <PostTitle>{post.frontmatter.title}</PostTitle>
+                <small>{post.frontmatter.date}</small>
+                <small>
+                  {post.frontmatter.image &&
+                    post.frontmatter.image.childImageSharp.fluid.src}
+                </small>
+              </PostTitleContainer>
+              <PostContentContainer>
+                {post.excerpt}
+                <br />
+                <br />
+                <Link className="button is-small" to={post.fields.slug}>
+                  Keep Reading →
+                </Link>
+              </PostContentContainer>
+            </StyledPostPreview>
+          ))}
         </section>
       </Layout>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -71,9 +95,16 @@ export const pageQuery = graphql`
             title
             templateKey
             date(formatString: "MMMM DD, YYYY")
+            full_image {
+              childImageSharp {
+                fluid {
+                  src
+                }
+              }
+            }
           }
         }
       }
     }
   }
-`
+`;
